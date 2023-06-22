3d1bc6006450abec363a1d4a3a93beec16c3104a
Ar7109 <lmsgt10@gmail.com>
2023-06-22 16:22:10 +0530

    Python_Turtle

---
+++ Desktop/Python_turtle/grpcs.py
@@ -0,0 +1,15 @@
+
+
+import turtle
+t = turtle.Turtle()
+
+t.speed(100)
+turtle.bgcolor("black")
+
+for i in range(240):
+    t.color("green")
+    t.circle(i)
+    t.left(502)
+    t.right(110)
+
+turtle.done()
